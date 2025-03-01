523e8ab66c6624c3e2a495984265cc127db87ca8
Alejandrette <alejandrette5117@gmail.com>
2025-03-01 14:26:07 +0100

    creader reducer

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,6 +1,18 @@
+import { ChangeEvent, useMemo, useState } from "react";
+
 export default function BudgetForm() {
+  const [budget, setBudget] = useState(0)
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setBudget(e.target.valueAsNumber)
+  }
+
+  const isValid: boolean = useMemo(() => {
+    return isNaN(budget) || budget <= 0
+  }, [budget])
+
   return (
-    <div className="flex items-center justify-center min-h-screen">
+    <div className="flex items-center justify-center mt-20">
       <div className="w-full max-w-lg p-8 bg-white shadow-lg rounded-2xl">
         <h1 className="text-2xl font-bold text-center text-gray-700">Define your budget</h1>
         
@@ -8,10 +20,12 @@ export default function BudgetForm() {
           <input 
             type="number" 
             placeholder="E.g. $5000"
-            className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-500"
+            value={budget}
+            onChange={handleChange}
+            className="w-full p-3 border text-black border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-teal-500 bg-transparent"
           />
           
-          <button className="w-full bg-teal-600 text-white py-3 rounded-lg font-semibold hover:bg-teal-700 transition duration-300">
+          <button className="w-full bg-teal-600 text-white py-3 rounded-lg font-semibold enabled:hover:bg-teal-700 disabled:opacity-60 transition duration-300" disabled={isValid}>
             Define your budget
           </button>
         </form>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header className="max-h-72 bg-amber-700 p-4">
+    <header className="max-h-72 bg-teal-700 p-4">
       <h1 className="text-3xl text-center font-bold">Expense Control</h1>
     </header>
   )
